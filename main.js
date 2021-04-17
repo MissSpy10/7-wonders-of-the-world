d459b81d2cca73582e66211f5784b242327c9b24
MissSpy10 <82704911+MissSpy10@users.noreply.github.com>
2021-04-17 15:30:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function one()
+{
+  window.alert("An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage.")
+}
+
+function two()
+{
+  window.alert("The statue of Christ the Redeemer was completed in 1931 and stands 98 feet (30 metres) tall, its horizontally outstretched arms spanning 92 feet (28 metres). The statue has become emblematic of both the city of Rio de Janeiro and the whole nation of Brazil. Christ the Redeemer statue, Rio de Janeiro.")
+}
+function three()
+{
+  window.alert("Machu Picchu is a 15th-century Inca citadel, located in the Eastern Cordillera of southern Peru, on a 2,430-metre (7,970 ft) mountain ridge. It is located in the Machupicchu District within Urubamba Province above the Sacred Valley, which is 80 kilometres (50 mi) northwest of Cuzco.")
+}
+
+function four()
+{
+  window.alert("Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs, earning its nickname, the Rose City. Perhaps its most famous structure is 45m-high Al Khazneh, a temple with an ornate, Greek-style facade, and known as The Treasury.")
+}
+
+function five()
+{
+  window.alert("Chichen Itza was a large Mayan city by the Maya civilization. It is on the Yucatán Peninsula, about 120 km to the east of Mérida.Some of the most famous parts of Chichen Itza include: the temple of Kukultán, the Plumed Serpent (pictured). This is called El Castillo: in Spanish. Every year on the Fall (Autumn) and Spring equinox the Sun hits the side of the building making a show of light and shadow which looks like a snake along the steps of the building. It has four sides, and each side has 91 steps.There are many temples and pyramids in Chichen Itza.")
+}
+
+function six()
+{
+  window.alert("The Colosseum is an amphitheatre built in Rome under the Flavian emperors of the Roman Empire. It is also called the Flavian Amphitheatre. It is an elliptical structure made of stone, concrete, and tuff, and it stands four stories tall at its highest point. It measures 620 by 513 feet (189 by 156 metres) and could hold as many as 50,000 spectators. The Colosseum was famously used for gladiatorial combat.")
+}
+
+function seven()
+{
+  window.alert("The Great Wall of China is an ancient wall in China. The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196 kilometers long, 9.1 metres (30 feet) wide and 15 metres high. The earlier sections on the wall are made of compacted dirt and stone. Later in the Ming Dynasty they used bricks. There are 7,000 watch towers, block houses for soldiers and beacons to send smoke signals.")
+}
